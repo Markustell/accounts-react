abae4607d7d2d496edf2e7377e3055c3d8814bec
Markustell <woraponklabsri7731@gmail.com>
2023-06-05 23:45:45 +0700

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import './Footer.css'
+
 function Footer() {
+
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer>
+      <p>copyright &copy; {currentYear} t31ly. All Rights Reserved</p>
+    </footer>
   )
 }
 
